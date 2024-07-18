dc059109b8e993393256629570e49d11e33df214
ajpelu <ajpelu@gmail.com>
2024-07-18 23:34:06 +0200

    document dataset

---
+++ R/bai_pinus.R
@@ -0,0 +1,16 @@
+#' Basal Area Index of Mediterranean *Pinus* species
+#'
+#' A dataset with Basal Area Index (bai) of several *Pinus* species plots and
+#' climate variables for each year.
+#'
+#' @format
+#' A data frame with 564 rows and 5 columns:
+#'
+#'  * `bai`: basal area index (\eqn{cm^2 \cdot year^{-1}}) of all trees of the studied plots
+#'  * `sp_code`: code of the species studied
+#'  * `prec`: annual precipitation (mm)
+#'  * `tmed`: annual mean temperature (º)
+#'  * `water_balance`: difference among the annual precipitation and the potential
+#' evapotransporation.
+#'
+"bai_pinus"
